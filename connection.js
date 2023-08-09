be1014a28001be75682a390a1cd19bc0b16602fc
cayomol01 <jacmg01@gmail.com>
2023-08-08 21:18:51 -0600

    commit final del día

---
+++ connection.js
@@ -0,0 +1,8 @@
+
+
+function menu(){
+    console.log('\n************ MENU ************');
+    console.log('1. Iniciar sesión');
+    console.log('2. Registrarse');
+    console.log('3. Salir');
+}
