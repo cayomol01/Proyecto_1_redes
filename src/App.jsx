e3870e4b061c9518bcb8d651c13794f05e3b02c5
cayomol01 <jacmg01@gmail.com>
2023-08-21 02:07:52 -0600

    pruebas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-const App = () =>{
+console.log("hola")
+
+
+function App(){
     return (
-        <h1>
+        <div>
+                    <h1>
             Welcome to React App thats build using Webpack and Babel separately
         </h1>
+        </div>
+
     )
 }
 
